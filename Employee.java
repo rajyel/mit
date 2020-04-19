1f03fa5952e0770e131d49c56af77419aa47a8c7
RajeshYelnare <rajyel@gmail.com>
2020-04-19 16:46:05 +0530

    test3

--- Employee.java
+++ Employee.java
@@ -3,6 +3,8 @@ class Employee{
 }  
 class Programmer extends Employee{  
  int bonus=10000;  
+ /** Test 
+ **/
  public static void main(String args[]){  
    Programmer p=new Programmer();  
    System.out.println("Programmer salary is:"+p.salary);  
